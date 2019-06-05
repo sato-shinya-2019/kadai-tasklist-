9430323250572975ae3b39d7a20e6b0618faa3fe
shinmaru <shindora12@gmail.com>
2019-06-05 08:03:22 +0000

    post3

--- database/migrations/2019_06_01_103206_add_post_tasks_table.php
+++ database/migrations/2019_06_01_103206_add_post_tasks_table.php
@@ -14,7 +14,7 @@ class AddPostTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->increments('id');
+            
             $table->integer('user_id')->unsigned()->index();
 
             // 外部キー制約
